532cc54aebc393d5a5d1cee85bdfe8c8b74c881d
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-15 14:33:19 +0200

    mvc

---
+++ EjerciciosU6/EjerciciosU6/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using EjerciciosU6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EjerciciosU6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index()
+        {
+            return "Hola soy Marco";
+        }
+
+        public int Numero()
+        {
+            return 33;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
