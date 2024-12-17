62323599cfa2d1fe9815e32e2334e07ca850b31c
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-12-17 13:37:43 +0100

    pa raul

--- AJAX/Pokemon/Functions.js
+++ AJAX/Pokemon/Functions.js
@@ -1,92 +1,50 @@
 window.onload = inicializaEventos;
 
 function inicializaEventos() {
-    pedirDatos()
+    mostrarDatos("https://pokeapi.co/api/v2/pokemon");
 }
 
-function buscarPokemon()
-{
+function mostrarDatos(url) {
+    var miLlamada = new XMLHttpRequest();
 
-let nombre = '';
+    miLlamada.open("GET", url);
 
-nombre = document.getElementById("nombre").value
-
-var miLlamada = new XMLHttpRequest();
-
-miLlamada.open("GET", "https://pokeapi.co/api/v2/pokemon" + "/" + nombre);
-
-//Definicion estados
-
-miLlamada.onreadystatechange = function () {
-
-if (miLlamada.readyState < 4) {
-
-//aquí se puede poner una imagen de un reloj o un texto “Cargando”
-
+    miLlamada.onreadystatechange = function () {
+        if (miLlamada.readyState < 4) {
+            // aquí se puede poner una imagen de un reloj o un texto “Cargando”
+        } else if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            var arrayPokemon = JSON.parse(miLlamada.responseText);
+            listarPokemon(arrayPokemon);
+        }
+    };
+    miLlamada.send();
 }
 
-else
+function listarPokemon(array) {
+    document.getElementById("desplegable").innerHTML = array.results.map((pokemon) =>
+        `<option id="${pokemon.name}" value="${pokemon.url}">
+            ${pokemon.name}
+        </option>`
+    ).join("");
 
-if (miLlamada.readyState == 4 && miLlamada.status == 200) {
-
-var arrayPokemon = JSON.parse(miLlamada.responseText);
-mostrarPokemon(arrayPokemon);
-
-
+    // Añadir evento change al select
+    document.getElementById("desplegable").addEventListener("change", function() {
+        mostrarPokemon(this.value);
+    });
 }
 
-};
+function mostrarPokemon(url) {
+    var miLlamada = new XMLHttpRequest();
+    miLlamada.open("GET", url);
 
-miLlamada.send();
-}
-
-function pedirDatos()
-
-{
-
-var miLlamada = new XMLHttpRequest();
-
-miLlamada.open("GET", "https://pokeapi.co/api/v2/pokemon");
-
-//Definicion estados
-
-miLlamada.onreadystatechange = function () {
-
-if (miLlamada.readyState < 4) {
-
-//aquí se puede poner una imagen de un reloj o un texto “Cargando”
-
-}
-
-else
-
-if (miLlamada.readyState == 4 && miLlamada.status == 200) {
-
-var arrayPokemon = JSON.parse(miLlamada.responseText);
-listarPokemon(arrayPokemon);
-
-
-}
-
-};
-
-miLlamada.send();
-}
-
-function listarPokemon(array){
-
-    document.getElementById("lista").innerHTML = array.results.map((pokemon) =>
-         `<li>
-            <a href="https://pokeapi.co/api/v2/pokemon/1/">${pokemon.name}</a>
-    </li> `
-).join("");
-}
-
-function mostrarPokemon(array){
-    document.getElementById("pokemon").innerHTML = array.results.map((pokemon) =>
-    `<p>${pokemon.name}</p>
-    <img src="${pokemon.sprites.front_default}">
-    
-    `
-    )
+    miLlamada.onreadystatechange = function () {
+        if (miLlamada.readyState < 4) {
+            // aquí se puede poner una imagen de un reloj o un texto “Cargando”
+        } else if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            var pokemon = JSON.parse(miLlamada.responseText);
+            console.log(pokemon);
+            // Aquí puedes mostrar los detalles del Pokémon
+        }
+    };
+    miLlamada.send();
 }
