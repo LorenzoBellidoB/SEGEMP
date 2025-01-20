8afaf6f339c4e9f39eeb431f3c69abf6e59b2559
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2025-01-20 14:18:39 +0100

    no fuimos

---
+++ Hubs/Shaaats/Models/ClsMensajeUsuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public class ClsMensajeUsuario
+    {
+        private string nombreUsuario;
+        private string mensajeUsuario;
+
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string MensajeUsuario { get => mensajeUsuario; set => mensajeUsuario = value; }
+    }
+}
---
+++ Hubs/Shaaats/NewSHAT/NewSHAT/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Models;
+namespace NewSHAT.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(ClsMensajeUsuario mensaje)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", mensaje.NombreUsuario, mensaje.MensajeUsuario);
+        }
+    }
+}
