6565454995b51d44ac77767b22621fc57cd5b317
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2025-01-28 14:34:23 +0100

    material

---
+++ Angular/FormularioMaterial/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { FormularioPersonaComponent } from './formulario-persona/formulario-persona.component';
+
+export const routes: Routes = [{ path: 'formulario', component: FormularioPersonaComponent }];
---
+++ Angular/FormularioMaterial/src/app/formulario-persona/formulario-persona.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatInputModule } from '@angular/material/input';
+
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-formulario-persona',
+  imports: [MatFormFieldModule, MatCardModule, MatInputModule, NgIf],
+  templateUrl: './formulario-persona.component.html',
+  styleUrl: './formulario-persona.component.css'
+})
+export class FormularioPersonaComponent implements OnInit {
+
+  formulario!:FormGroup;
+  
+
+  ngOnInit(): void {
+
+  this.formulario=new FormGroup(
+
+  {
+    nombre: new FormControl('',[Validators.required]),
+    apellidos:new FormControl('',[])
+  });
+  }
+
+  saluda(){
+
+    if (this.formulario.valid)
+      alert('Hola ' + this.formulario.controls.nombre.value + ' ' + this.formulario.controls.apellidos.value);
+
+  }
+}
